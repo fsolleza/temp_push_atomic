3ec99e6465850a3ad471d748ef2a2f16c5a53993
vic-shihang-li <viclee@nyu.edu>
2021-11-19 12:54:09 -0500

    update unsync reader impl

--- src/main.rs
+++ src/main.rs
@@ -85,7 +85,7 @@ impl Data {
                 break;
             }
             self.data[idx] = data[idx % data.len()];
-            fence(Ordering::Release);
+            // fence(Ordering::Release);
             self.idx += 1;
             if idx % sync_freq == 0 {
                 self.atomic_idx.store(self.idx, Ordering::SeqCst);
@@ -217,14 +217,14 @@ fn run_seqcst_reader(data: &Data) {
     }
 }
 
-fn run_unsynchronized_reader(data: &Data) -> Result<(), &'static str> {
+fn run_unsynchronized_reader(data: &Data) {
     let l = data.data.len();
     loop {
-        if data.idx > 0 && data.data[data.idx - 1] == 0 {
-            return Err("Reader runs faster than writer!");
+        if data.idx > 0 {
+            assert!(data.data[data.idx - 1] > 0);
         }
         if data.idx == l {
-            return Ok(());
+            break;
         }
     }
 }
@@ -326,53 +326,53 @@ fn main() {
     //let args: Vec<String> = env::args().collect();
     let len: usize = 10_000_000;
 
-    println!("\nNONE");
-    let arr = new_data();
-    let mut data = Data::new(len);
-    data.push(&arr[..]);
-    let d = data.sum();
-    println!("{}", d);
+    // println!("\nNONE");
+    // let arr = new_data();
+    // let mut data = Data::new(len);
+    // data.push(&arr[..]);
+    // let d = data.sum();
+    // println!("{}", d);
 
-    println!("\nMUTEX");
-    let arr = new_data();
-    let mut data = Data::new(len);
-    data.mutex_push(&arr[..]);
-    let d = data.sum();
-    println!("{}", d);
+    // println!("\nMUTEX");
+    // let arr = new_data();
+    // let mut data = Data::new(len);
+    // data.mutex_push(&arr[..]);
+    // let d = data.sum();
+    // println!("{}", d);
 
-    println!("\nSEQCST");
-    let arr = new_data();
-    let mut data = Data::new(len);
-    data.atomic_seqcst_push(&arr[..]);
-    let d = data.sum();
-    println!("{}", d);
+    // println!("\nSEQCST");
+    // let arr = new_data();
+    // let mut data = Data::new(len);
+    // data.atomic_seqcst_push(&arr[..]);
+    // let d = data.sum();
+    // println!("{}", d);
 
-    println!("\nRELAXED");
-    let arr = new_data();
-    let mut data = Data::new(len);
-    data.atomic_fenced_push(&arr[..]);
-    let d = data.sum();
-    println!("{}", d);
+    // println!("\nRELAXED");
+    // let arr = new_data();
+    // let mut data = Data::new(len);
+    // data.atomic_fenced_push(&arr[..]);
+    // let d = data.sum();
+    // println!("{}", d);
 
-    println!("\nATOMIC BOOL ARRAY");
-    let arr = new_data();
-    let mut data = Data::new(len);
-    data.atomic_bool_push(&arr[..]);
-    let d = data.sum();
-    println!("{}", d);
+    // println!("\nATOMIC BOOL ARRAY");
+    // let arr = new_data();
+    // let mut data = Data::new(len);
+    // data.atomic_bool_push(&arr[..]);
+    // let d = data.sum();
+    // println!("{}", d);
 
-    println!("\nMUTEX WRITE+READ");
-    run_mutex_push_read(len, 4);
+    // println!("\nMUTEX WRITE+READ");
+    // run_mutex_push_read(len, 4);
 
-    println!("\nSEQCST WRITE+READ");
-    run_seqcst_push_read(len, 4);
+    // println!("\nSEQCST WRITE+READ");
+    // run_seqcst_push_read(len, 4);
 
     println!("\nOPTIONAL SYNC WRITE+READ");
     run_optional_sync_push_read(len, 4, 5);
 
-    println!("\nFENCED WRITE+READ");
-    run_fenced_push_read(len, 4);
+    // println!("\nFENCED WRITE+READ");
+    // run_fenced_push_read(len, 4);
 
-    println!("\nATOMIC BOOL WRITE+READ");
-    run_bool_push_read(len, 4);
+    // println!("\nATOMIC BOOL WRITE+READ");
+    // run_bool_push_read(len, 4);
 }
